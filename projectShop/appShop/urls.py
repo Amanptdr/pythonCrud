6df41d56bbbaec46aa2558dd44249609c4411167
Aman Patidar <suyashpandey.synergytop@gmail.com>
2023-09-05 18:58:32 +0530

    remove commented code

--- projectShop/appShop/urls.py
+++ projectShop/appShop/urls.py
@@ -7,13 +7,7 @@ urlpatterns = [
          path('test/', views.Testing.as_view(), name ='test'),
          path('home/', views.HomeView.as_view(), name ='home'),
          path('logout/', views.LogoutView.as_view(), name ='logout'),
-         path('login/', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
+         path('auth-token/', views.MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
          path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
          path('register/', views.RegisterView.as_view(), name='auth_register'),
-     #     path('register/', views.UserRegistrationView.as_view(), name ='register')
          ]
-    # path('index/', views.ApiOverview, name='home'),
-    # path('create/', views.add_category, name='add-category'),
-    # path('view/', views.view_category, name='view_items'),
-    # path('update/<int:pk>/', views.update_category, name='update-items'),
-    # path('category/<int:pk>/delete/', views.delete_category, name='delete-items'),
--- projectShop/appShop/views.py
+++ projectShop/appShop/views.py
@@ -1,14 +1,11 @@
-from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import UserRegistrationSerializer,MyTokenObtainPairSerializer,RegisterSerializer
 from rest_framework import generics
 from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
 
 class HomeView(APIView):
      
@@ -24,14 +21,6 @@ class Testing(APIView):
 
 class MyObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
-    # def getUserDetails(request):
-    #     username = request.POST["username"]
-    #     password = request.POST["password"]
-    #     userDetails = authenticate(request, username=username, password=password)
-    #     if user is not None:
-    #         return userDetails
-    #     else:
-    #         return None
     serializer_class = MyTokenObtainPairSerializer
 
 class RegisterView(generics.CreateAPIView):
@@ -40,7 +29,6 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 class UserRegistrationView(APIView):
     def post(self, request):
-        print(request.data,"1222222222222")
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -57,65 +45,3 @@ class LogoutView(APIView):
                return Response(status=status.HTTP_205_RESET_CONTENT)
           except Exception as e:
                return Response(status=status.HTTP_400_BAD_REQUEST)
-# @api_view(['GET'])
-# def ApiOverview(request):
-#     api_urls = {
-#         'all_items': '/',
-#         'Search by Category': '/?category=category_name',
-#         'Search by Subcategory': '/?subcategory=category_name',
-#         'Add': '/create',
-#         'Update': '/update/pk',
-#         'Delete': '/item/pk/delete'
-#     }
- 
-#     return Response(api_urls)
-# @api_view(['POST'])
-# def add_category(request):
-#     item = CategorySerializer(data=request.data)
- 
-#     # validating for already existing data
-#     if Category.objects.filter(**request.data).exists():
-#         raise serializers.ValidationError('This data already exists')
- 
-#     if item.is_valid():
-#         item.save()
-#         return Response(item.data)
-#     else:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-# @api_view(['GET'])
-# def view_category(request):
-	
-	
-# 	# checking for the parameters from the URL
-# 	if request.query_params:
-# 		items = Category.objects.filter(**request.query_params.dict())
-# 	else:
-# 		items = Category.objects.all()
-# 	# if there is something in items else raise error
-# 	if items:
-# 		serializer = CategorySerializer(items, many=True)
-# 		return Response(serializer.data)
-# 	else:
-# 		return Response('status=status.HTTP_404_NOT_FOUND')
-
-
-
-# @api_view(['POST'])
-# def update_category(request, pk):
-#     item = Category.objects.get(pk=pk)
-#     data = CategorySerializer(instance=item, data=request.data)
- 
-#     if data.is_valid():
-#         data.save()
-#         return Response(data.data)
-#     else:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-
-
-# @api_view(['DELETE'])
-# def delete_category(request, pk):
-#     item = get_object_or_404(Category, pk=pk)
-#     item.delete()
-#     return Response(status=status.HTTP_202_ACCEPTED)
